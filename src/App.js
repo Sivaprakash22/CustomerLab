6858590a2578d8cf3aa7877e8460a27884e3a1dd
Sivaprakash22 <sivaprakash138@gmail.com>
2021-07-24 18:50:08 +0530

    server fetch completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Popup from "./Components/PopUp"
 import "./App.css";
+import { useState } from "react";
 function App() {
+
+  const [open,setOpen] = useState(false); 
+
   return (
     <div  className="App">
        <div className="navBar">
            <h2>View Audience</h2>
        </div>
-       <input className="btn"  type="button" value="Save Segment" />
-       <Popup />
+       <input className="btn" onClick={()=>setOpen(true)}  type="button" value="Save Segment" />
+       {
+         (open) ? <Popup control={setOpen} /> : ""
+       }
     </div>
   );
 }
--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -1,67 +1,151 @@
-import { useState,useEffect } from "react";
-import "./Popup.css"
-function Popup() {
+import {useState} from "react";
+import "./Popup.css";
+function Popup(props) {
+  
+ //raw data
+  const segmentValues = {
+                         first_name : "First Name",
+                         last_name : "Last Name",
+                         gender : "Gender",
+                         age : "Age",
+                         account_name : "Account Name",
+                         city : "City",
+                         state : "State"
+                        };
+  
+  
+  const [segmentName, setName] = useState('');                     
+  const [segments, setSegments] = useState(Object.values(segmentValues));
+  const [blueBox, setBlueBox] = useState([]);
 
- const [segments,setSegments] = useState(["First Name", "Last Name","Gender"," Age","Account Name","City","State"]) 
- const [blueBox,setBlueBox] = useState([segments]);
 
- const addDropDownToBlueBox =()=>{
-   let newDropDown = blueBox;
-   newDropDown.push(segments);
-   console.log(newDropDown);
-   setBlueBox([...newDropDown]);
- }
+   const addToBlueBoxWithValue = (selectedValue) => {
+  
+    //filtering the segments
+    const remainingSegments = segments.filter((value) => value !== selectedValue);
+    
+    //Creating dropdown
+    let newDropDown = blueBox;
+    newDropDown.push(selectedValue);
 
-  // useEffect(()=>{
-     
-  // },[blueBox])
+    //Adding data to Blue Box 
+    setBlueBox(newDropDown);
+    setSegments(remainingSegments);
+  };
 
+
+  
+  const sendToServer = async() => {
+
+    //constructing data for server
+    const data = {};
+    data.segment_name = segmentName;
+    data.schema = [];
+
+    Object.keys(segmentValues).forEach(key =>{
+    blueBox.forEach(value => {
+           if(segmentValues[key]===value)
+           {
+             const obj = {}
+             obj[key] = value;
+             data.schema.push(obj);
+           }
+      })
+    })
+ 
+   //Server Data
+   //console.log(data);
+
+   fetch('https://webhook.site/12a9a4fa-3239-48f8-b433-c72532a79eba',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    }).then(res=>console.log(res));
+    
+  }
 
   return (
     <div className="overlay">
-    <div className="popup">
-      <div className="header">
+      <div className="popup">
+        <div className="header">
           <h3>Saving Segment</h3>
-      </div>
-      <div className="container">
-         <p>Enter the Name of the Segment</p>
-         <input className="textBox" type="text"  defaultValue="" placeholder=" Name of the Segment"/>
-         <p>To save your segment, you need to add the schemas to build  the query</p>
-         <div className="group">
-             <p id="green"> - User Traits</p>
-             <p id="red"> - Group Traits</p>
-         </div>
-
-        <div className="blueBox">
-        {
-           blueBox.map((dropDown,index) =>
-            <select>
-                  <option key={index} defaultValue="Add schema to segment" selected hidden className="opt">Add schema to segment</option>
-                  {
-                    dropDown.map(dropDownItem=><option key={index+dropDownItem} className="opt" defaultValue={dropDownItem}>{dropDownItem}</option>)
-                  }
-            </select>)
-        }
         </div>
+        <div className="container">
+          <p>Enter the Name of the Segment</p>
+          <input
+            className="textBox"
+            type="text"
+            onChange={(e)=>setName(e.target.value)}
+            defaultValue={segmentName}
+            placeholder=" Name of the Segment"
+          />
+          <p>
+            To save your segment, you need to add the schemas to build the query
+          </p>
+          <div className="group">
+            <p id="green"> - User Traits</p>
+            <p id="red"> - Group Traits</p>
+          </div>
+          <div className="blueBox">
+            {
+            (blueBox.length!==0) ? (blueBox.map((val, index) => (
+              <select>
+                <option
+                  key={index}
+                  defaultValue={val}
+                  selected
+                  hidden
+                  className="opt"
+                >
+                  {val}
+                </option>
+                {segments.map((dropDownItem) => (
+                  <option
+                    key={index + dropDownItem}
+                    className="opt"
+                    defaultValue={dropDownItem}
+                  >
+                    {dropDownItem}
+                  </option>
+                ))}
+              </select>
+            ))) : <p>No Segments added</p>}
+          </div>
 
-        <select >
-             <option defaultValue="Add schema to segment" selected hidden className="opt">Add schema to segment</option>
-             <option className="opt">First Name</option>
-        </select>
+          <select id="selectedSegment">
+            <option
+              defaultValue="Add schema to segment"
+              selected
+              hidden
+              className="opt"
+            >
+              Add schema to segment
+            </option>
+            {segments.map((listItem, index) => (
+              <option
+                className="opt"
+                key={index + listItem}
+                
+                defaultValue={listItem}
+              >
+                {listItem}
+              </option>
+            ))}
+          </select>
 
+          <button className="addBtn" onClick={(e) => addToBlueBoxWithValue(document.getElementById("selectedSegment").value)}>
+            {" "}
+            + Add new schema
+          </button>
 
-        <button className="addBtn" onClick={()=>addDropDownToBlueBox()} > + Add new schema</button>
-
-       <div className="footer">
-       <button id="save">Save the segment</button>
-       <button id="cancel">Cancel</button>
-       </div>
+          <div className="footer">
+            <button id="save" onClick={()=> sendToServer()} >Save the segment</button>
+            <button id="cancel" onClick={()=>props.control(false)}>Cancel</button>
+          </div>
+        </div>
       </div>
-
-     
-  
-      
-    </div>
     </div>
   );
 }
